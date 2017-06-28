67739ad40a1527c05bc870a2de7bd5a67698f065
Jiaying Ren <jiaying.ren@umcloud.com>
2017-06-28 11:25:57 +0800

    rgw: subuser usage log show
    Signed-off-by: Jiaying Ren <jiaying.ren@umcloud.com>

--- src/rgw/rgw_log.cc
+++ src/rgw/rgw_log.cc
@@ -207,10 +207,6 @@ static void log_usage(struct req_state *s, const string& op_name)
     bucket_name = "-"; /* bucket not found, use the invalid '-' as bucket name */
   }
 
-  string u = user.to_str();
-  string p = payer.to_str();
-  rgw_usage_log_entry entry(u, p, bucket_name);
-
   uint64_t bytes_sent = s->cio->get_bytes_sent();
   uint64_t bytes_received = s->cio->get_bytes_received();
 
@@ -220,10 +216,24 @@ static void log_usage(struct req_state *s, const string& op_name)
   if (!error)
     data.successful_ops = 1;
 
+  string u = user.to_str();
+  string p = payer.to_str();
+
+  if (!u.empty() && s->cct->_conf->rgw_enable_usage_log_at_subuser_level) {
+    const auto subuser = s->subuser;
+    if (!(subuser.empty())) {
+      ldout(s->cct, 10) << "subuser usage log subuser=" << subuser << dendl;
+      rgw_usage_log_entry sentry(u, p, bucket_name);
+      utime_t sts = ceph_clock_now(s->cct);
+      sentry.add(subuser, op_name, data);
+      usage_logger->insert(sts, sentry);
+    }
+  } 
+
+  rgw_usage_log_entry entry(u, p, bucket_name);
   entry.add(op_name, data);
 
   utime_t ts = ceph_clock_now(s->cct);
-
   usage_logger->insert(ts, entry);
 }
 
