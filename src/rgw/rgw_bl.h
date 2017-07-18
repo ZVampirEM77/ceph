0eea1099a900bbcb91efe5766de5b531b7c2fc2c
Enming Zhang <enming.zhang@umcloud.com>
2017-07-18 15:04:17 +0800

    rgw: bl amend delivering strategy
    Support to deliver the bucket log multi-times one day
    
    Signed-off-by: Enming Zhang <enming.zhang@umcloud.com>

--- src/rgw/rgw_bl.h
+++ src/rgw/rgw_bl.h
@@ -36,6 +36,7 @@
 using namespace std;
 #define BL_HASH_PRIME 7877
 #define BL_UNIQUE_STRING_LEN 16
+#define BL_DELIVER_INTERVAL 86400
 static string bl_oid_prefix = "bl";
 static string bl_index_lock_name = "bl_process";
 
@@ -74,6 +75,7 @@ class RGWBL {
 
  public:
   BLWorker *worker;
+  time_t deliver_interval;
   RGWBL() : cct(nullptr), store(nullptr), worker(nullptr) {}
   ~RGWBL() {
     stop_processor();
